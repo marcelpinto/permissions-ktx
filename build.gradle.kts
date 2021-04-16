7e997a6ad84f47a5a6309ca604b22f17e2b0297d
Erik Eelde <erik.eelde@gmail.com>
2021-04-16 09:16:00 +0200

    Bump gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha09")
+        classpath("com.android.tools.build:gradle:7.0.0-alpha14")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
     }
 }
