ec9ef51644ce8b5b50e93c4db0812bd08fa98fd2
marcelpinto <marcelpinto@google.com>
2021-09-27 13:05:29 +0800

    PermissionProvider tests would fail when run together
    The PermissionProvider instance would be already initialized from other test
    Thus it would fail the condition.
    
    Move instance to backing property and create internal clear method.

--- lib/src/test/java/dev/marcelpinto/permissionktx/PermissionProviderTest.kt
+++ lib/src/test/java/dev/marcelpinto/permissionktx/PermissionProviderTest.kt
@@ -2,10 +2,10 @@ package dev.marcelpinto.permissionktx
 
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.flow.emptyFlow
+import org.junit.Before
 import org.junit.Test
 
 class PermissionProviderTest {
-
 
     private val fakeChecker = object : PermissionChecker {
         override fun getStatus(type: Permission) = PermissionStatus.Granted(Permission("any"))
@@ -16,10 +16,20 @@ class PermissionProviderTest {
         override fun refreshStatus() {}
     }
 
+    @Before
+    fun setUp() {
+        PermissionProvider.clear()
+    }
+
     @Test
     fun `test isInitialized when PermissionProvider is initialized`() {
         assertThat(PermissionProvider.isInitialized()).isFalse()
         PermissionProvider.init(fakeChecker, dummyObserver)
         assertThat(PermissionProvider.isInitialized()).isTrue()
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `test accessing instance without init throws exception`() {
+        PermissionProvider.instance
     }
 }
